c28dc67917196dbf30269078b9139993c1beb792
pcampos <pcampos119104@gmail.com>
2023-08-02 08:36:56 -0300

    chap 10

--- django_project/settings.py
+++ django_project/settings.py
@@ -146,3 +146,4 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "admin@djangobookstore.com"
